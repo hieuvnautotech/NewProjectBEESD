2fcdb17cc10185834cdf6f8b3cd7f96c7b07515a
hieu <hieuvnautotech@gmail.com>
2023-11-29 22:09:11 +0700

    ok

--- NewProjectESDBETL/NewProjectESDBETL/Services/Common/LoginService.cs
+++ NewProjectESDBETL/NewProjectESDBETL/Services/Common/LoginService.cs
@@ -1,4 +1,5 @@
-﻿using NewProjectESDBETL.Models.Dtos.Common;
+﻿using Dapper;
+using NewProjectESDBETL.Models.Dtos.Common;
 
 namespace NewProjectESDBETL.Services.Common
 {
@@ -7,9 +8,27 @@ namespace NewProjectESDBETL.Services.Common
         Task<string> CheckLogin(LoginModelDto model);
         Task<ResponseModel<IEnumerable<UserDto>?>> GetOnlineUsers();
     }
+
+    [SingletonRegistration]
     public class LoginService : ILoginService
     {
+        private readonly ISqlDataAccess _sqlDataAccess;
 
+        public LoginService(ISqlDataAccess sqlDataAccess)
+        {
+            _sqlDataAccess = sqlDataAccess;
+        }
+
+        public async Task<string> CheckLogin(LoginModelDto model) {
+            model.userPassword = MD5Encryptor.MD5Hash(model.userPassword);
+            string proc = "sysUsp_User_CheckLogin";
+            var param = new DynamicParameters();
+            param.Add("@userName", model.userName);
+            param.Add("@userPassword", model.userPassword);
+
+            var data = await _sqlDataAccess.LoadDataUsingStoredProcedure<string>(proc, param);
+            return data.FirstOrDefault() ?? string.Empty;
+        }
     }
 
     
